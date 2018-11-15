d2f5dc1c60e37b69bb3b5d94d111c7e37b21c7bd
Raj-7799 <raj1471997@gmail.com>
2018-11-15 21:45:39 +0530

    preliminary solution

--- Two Sum.py
+++ Two Sum.py
@@ -7,8 +7,30 @@ class Solution:
         """
         nums = sorted(nums)
         
+        for i in range(len(nums) - 1):
+            found = self.search(nums, target - nums[i], i + 1, len(nums))
+            if found > -1:
+                return [i, found]
+
     
 
-    def search(self, num, find):
-        
+    def search(self, num, find, start, end):
+        if start <= end:
+            mid = (start + end - 1) // 2
+            if num[mid] == find:
+                return mid
+            else:
+                if num[mid] > find:
+                    return self.search(num, find, start, mid)
+                
+                else:
+                    return self.search(num, find, mid, end)
+            
+        else:
+            return -1
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.twoSum([3,2,3], 6))
 
