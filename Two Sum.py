2471eac66af64368dda34371475aded321da88b6
Raj-7799 <raj1471997@gmail.com>
2018-11-15 21:31:35 +0530

    Adding Two Sum file

---
+++ Two Sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums = sorted(nums)
+        
+    
+
+    def search(self, num, find):
+        
+
