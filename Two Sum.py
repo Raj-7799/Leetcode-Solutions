a1501c7361626901ab3c56296f971073d39b92e9
Raj-7799 <raj1471997@gmail.com>
2018-11-15 21:57:53 +0530

    new solution

--- Two Sum.py
+++ Two Sum.py
@@ -5,32 +5,15 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        nums = sorted(nums)
-        
+        sortedNums = sorted(nums)
+
         for i in range(len(nums) - 1):
-            found = self.search(nums, target - nums[i], i + 1, len(nums))
+            found = self.search(sortedNums, target - nums[i], i + 1, len(nums))
             if found > -1:
                 return [i, found]
 
     
 
-    def search(self, num, find, start, end):
-        if start <= end:
-            mid = (start + end - 1) // 2
-            if num[mid] == find:
-                return mid
-            else:
-                if num[mid] > find:
-                    return self.search(num, find, start, mid)
-                
-                else:
-                    return self.search(num, find, mid, end)
-            
-        else:
-            return -1
-
-
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.twoSum([3,2,3], 6))
-
+    print(solution.twoSum([3, 2, 4], 6))
