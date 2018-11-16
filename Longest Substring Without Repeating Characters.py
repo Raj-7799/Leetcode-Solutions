7e3b1bd2d1742bda5acf4788f5d97e603abfee1c
Raj-7799 <raj1471997@gmail.com>
2018-11-16 15:15:52 +0530

    try again 3.2

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -9,19 +9,19 @@ class Solution:
         count = 0
         maxCount = 0
         d = dict()
+        # print(len(charList))
 
         for c in charList:
             if c not in d:
                 count += 1
             else:
                 d = dict()
-                
                 maxCount = count if count > maxCount else maxCount
-                count = 0
+                count = 1
             d[c] = True
-        return maxCount
+        return max(maxCount, count)
 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.lengthOfLongestSubstring("pwwkew"))
+    print(s.lengthOfLongestSubstring("dvdf"))
