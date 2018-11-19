febda0e922948969350c538ea688ee743688cf43
Raj-7799 <raj1471997@gmail.com>
2018-11-20 02:12:01 +0530

    4.1 Initial solution

---
+++ Longest Palindromic Substring.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def checkPalindrome(self, s):
+        if s == s[::-1]:
+            return True
+        else:
+            return False
+
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        charArr = list(s)
+        output = ""
+        size = 0
+
+        for i in range(len(charArr) - 1):
+            p = ""
+            for i in range(i + 1, len(charArr)):
+                p += charArr[i]
+                
+                if self.checkPalindrome(p):
+                    if len(p) > size:
+                        output = p
+                        size = len(p)
+
+        return output
+
+    
+if __name__ == "__main__":
+    s = Solution()
+    print(s.longestPalindrome("abba"))
