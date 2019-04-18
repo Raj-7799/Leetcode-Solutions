3fe491be4a965ed47e915e9761a4d34c5771c90b
Raj Patel <raj1471997@gmail.com>
2019-04-18 22:32:38 +0530

    Create Transpose Matrix.py

---
+++ Transpose Matrix.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def transpose(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[A[j][i] for j in range(len(A))] for i in range(len(A[0]))]
