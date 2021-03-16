bea5899e4c4964e07718f9e7ed1b79721ef7928c
Raj <raj1471997@gmail.com>
2021-03-16 22:55:17 +0530

    Removing older files

--- Array Partition I.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        ans = 0
-        for i in range(len(nums) // 2):
-            ans += nums[2 * i]
-            
-        return ans
--- Stone Game.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def stoneGame(self, piles):
-        """
-        :type piles: List[int]
-        :rtype: bool
-        """
-        return True
